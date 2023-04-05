6e2be6aac25daf38b258cc525842ad535f666a66
sfulle14 <steven.fuller96@gmail.com>
2023-04-04 20:45:41 -0500

    add code to install libraries on each new computer

--- nTransportWithGUI.py
+++ nTransportWithGUI.py
@@ -1,6 +1,22 @@
 # Steven Fuller
 # June 9, 2020
 
+#importing libraries to install dependent libraries.
+import subprocess
+import sys
+
+# Defining a function that will be used to install dependent libraries
+# Function will take 1 argument that is the dependent library to install
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+# Calling function to install each dependent library
+install("matplotlib")
+install("numpy")
+install("python-math")
+install("tk")
+
+
 import matplotlib
 import numpy as np
 import math
